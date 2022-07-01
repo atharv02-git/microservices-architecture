5b6feeca799e42dd70fcb3eddb2d1a2132c00fe3
Atharv <atharvbhandare2001@gmail.com>
2022-07-01 18:51:54 +0530

    Comment service creation

---
+++ blog/comments/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  //We're going to check to see if we've already got some array inside commentsByPostId for the given post ID
+  //   i.e check whether any comment is assosciated with the given post id
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("Express server listening on port 4001");
+});
